0c340b77bf2d0994cdfe2f8e8faf04c9e311d6a4
Lukas Auerbeck <lauerbeck@gmx.at>
2016-03-18 19:37:02 +0100

    admin

--- admin/admin.php
+++ admin/admin.php
@@ -9,11 +9,51 @@
 			<ul>
 				<li class="nav-item"><a href="#">Personen bestätigen</a></li>				
 			</ul>
-		</div>
-
+		</div>		
 		<div class="main-content">
-			
-		</div>
+			<form action="update_data.php">
+
+				<table>
+					<tr>
+						<th>Vorname</th>
+						<th>Nachname</th>
+						<th>Username</th>
+						<th>E-Mail</th>
+						<th>Telefon Nr.</th>
+						<th>Straße</th>
+						<th>Ort</th>
+						<th>PLZ</th>
+						<th>Geburtstag</th>						
+					</tr>
+
+					<tr>
+						<td>Maria</td>
+						<td>Cole</td>
+						<td>mariacole</td>
+						<td>mariacole@inbound.plus</td>
+						<td>0123523452345</td>
+						<td>Waldstrasse 45</td>
+						<td>Feldbach</td>
+						<td>5221 </td>
+						<td>01.12.3010</td>
+					</tr>
+
+				</table>
+				<!-- <ul>
+					<li>						
+					Lillian Watts <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Ramiro Fleming <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Emmett Lyons <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Felipe Barrett <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Pauline Banks <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Pat Wheeler <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Anthony Walton <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Pablo Taylor <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Joey Strickland <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+					<li>Miriam Stokes <input type="checkbox" class="activate-checkbox" name="activate-user-id" ></li>
+				</ul> -->
+			</form>	
+		</div>		
 	</div>
 </body>
 </html>
