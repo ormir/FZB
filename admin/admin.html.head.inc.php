be96bc1d304303cb5604af018a01c421f4281305
Lukas Auerbeck <lauerbeck@gmx.at>
2016-03-18 18:53:24 +0100

    admin

---
+++ admin/admin.html.head.inc.php
@@ -0,0 +1,3 @@
+	<meta charset="UTF-8">
+	<title>Admin Panel</title>
+	<link rel="stylesheet" href="admin.css">
---
+++ admin/admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include("admin.html.head.inc.php"); ?>
+</head>
+<body>
+	<div class="site-wrap">
+		<div class="sidebar">
+			<ul>
+				<li class="nav-item"><a href="#">Personen bestätigen</a></li>				
+			</ul>
+		</div>
+
+		<div class="main-content">
+			
+		</div>
+	</div>
+</body>
+</html>
